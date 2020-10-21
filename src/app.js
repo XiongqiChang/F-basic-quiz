5da3a2d71f81fabbd53178dbbca2cc22244dfd21
XiongqiChang <2838159948@qq.com>
2020-10-21 13:56:02 +0800

    前后端联调成功

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 import "./style/index.scss";
+const axios = require("axios");
+const url = "http://localhost:8080/hello";
+
+const getInformation = async () => {
+    const data = await axios.get(url).then(response => {
+        console.log(response)
+    })
+   
+}
+
+getInformation()
+export {getInformation}
